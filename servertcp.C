836262eb6e9b3107ffdd3895b5b9c1f18ad55126
Felipe Martínez Nicholls <gopimn@gmail.com>
2016-03-07 16:33:01 -0300

    Nombre del archivo de registro
    Falta delimitar la cantidad de bytes recibidos.

--- servertcp.C
+++ servertcp.C
@@ -7,18 +7,26 @@
 #include <sys/types.h> 
 #include <sys/socket.h>
 #include <netinet/in.h>
-
+#include <time.h>
 void error(const char *msg)
 {
     perror(msg);
     exit(1);
 }
-
+signed long c_t(long value) {
+  if (value > 8388607) {
+    value = value - 16777216;
+  }
+  return value;
+}
 int main(int argc, char *argv[])
 {
      int sockfd, newsockfd, portno;
      socklen_t clilen;
-     char buffer[256];
+     char buffer[256],x[6],y[6],z[6];
+     char fileName[64];
+     time_t t = time(NULL);
+     struct tm tm  = *localtime(&t);
      struct sockaddr_in serv_addr, cli_addr;
      int n;
      FILE *fp;
@@ -26,7 +34,9 @@ int main(int argc, char *argv[])
          fprintf(stderr,"ERROR, no port provided\n");
          exit(1);
      }
-     
+     /*BEGIN OF NAMING*/
+ sprintf(fileName,"TEST_A3-%d-%d-%dT%d-%d-%d.txt", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec);
+     /*END OF NAMING*/
      sockfd = socket(AF_INET, SOCK_STREAM, 0);
      if (sockfd < 0) 
         error("ERROR opening socket");
@@ -46,28 +56,36 @@ int main(int argc, char *argv[])
                  &clilen);
      if (newsockfd < 0) 
        error("ERROR on accept");
-
      /*SE SUPONE QUE AQUI YA SE HIZO EL HANDSHAKE*/
      int message_counter=0;
-     while(1){
-       printf("Waiting for data\n");
-       bzero(buffer,256);
-       n = read(newsockfd,buffer,50);
-       if (n < 0) error("ERROR reading from socket");
-       printf("Here is the message number %d:\n%s\n",message_counter++,buffer);
-       fp=fopen("A3_recieve_turn_tcp32.txt", "a+");
-       fprintf(fp, "%s\n",buffer);
+       printf("Se escribira en el archivo %s \n",fileName);
+ printf("Waiting for data\n");
+       while(1){
+       bzero(buffer,29);
+       n = read(newsockfd,buffer,29);
+       if (n < 0) error("ERROR reading from socket ");
+       printf("%d:%s\n",message_counter++,buffer);
+       fp=fopen(fileName,"a+");
+       fprintf(fp, "%s",buffer);
+       fprintf(fp, "\n");
        fclose(fp);
-       if (message_counter==2000000){
-	 n = write(newsockfd,"C",1);
-	 if (n < 0) error("ERROR writing to socket, closing conection anyway");
-	 break;
+       if (message_counter==10000){
+	 printf("CAPTURA TERMINADA M Y GIGAS!\n");
+	 printf("Se escribira en el archivo %s \n",fileName);
+       	 n = write(newsockfd,"C",1);
+       	 if (n < 0) error("ERROR writing to socket, closing conection anyway");
+       	 break;
        }
-       /*END OF COMM*/
+       //      END OF COMM
      }
      close(newsockfd);
-
-     
      close(sockfd);
      return 0; 
 }
+
+
+/*
+que es sincronizado
+20 % complejidad con 1 ms
+ptp -> 
+*/
